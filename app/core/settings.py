e3d105059b6daedb636184199f40d8d06e33afd5
ocm <v.turukin@ocm-expert.com>
2025-03-27 17:58:41 +0300

    feat: settings envs updated

--- app/core/settings.py
+++ app/core/settings.py
@@ -5,6 +5,9 @@ from pydantic_settings import BaseSettings
 
 class BaseConfig(BaseSettings):
     app_title: str = 'Bet Marker'
+    postgres_user: str = 'postgres'
+    postgres_password: str = 'postgres'
+    postgres_db: str = 'postgres'
 
     class Config:
         env_file = '.env'
