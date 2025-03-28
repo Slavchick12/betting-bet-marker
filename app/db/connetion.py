fc42f62add727ef003ba4efb689fc20eb7bc34d0
ocm <v.turukin@ocm-expert.com>
2025-03-28 12:24:22 +0300

    feat: db connection created

---
+++ app/db/connetion.py
@@ -0,0 +1,15 @@
+"""Connection to database."""
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.settings import settings
+
+
+engine = create_async_engine(str(settings.postgres_dsn), echo=True, future=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
