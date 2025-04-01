bdf9e297d865fbfd0d24ccafe4f34aa81a9c345f
ocm <v.turukin@ocm-expert.com>
2025-04-01 10:03:26 +0300

    feat: /bets/ router created

--- app/api/routers/__init__.py
+++ app/api/routers/__init__.py
@@ -2,9 +2,10 @@
 
 from fastapi import APIRouter
 
+from .bets import router as bets_router
 from .events import router as events_router
 
 router = APIRouter()
 
+router.include_router(bets_router, prefix='/bets')
 router.include_router(events_router, prefix='/events')
-# router.include_router(bets_router, prefix='/bets')
