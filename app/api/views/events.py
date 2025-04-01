f98e2b78f407a71c326b73b5a28c4935a390df03
ocm <v.turukin@ocm-expert.com>
2025-04-01 10:04:22 +0300

    feat: view for update status created

--- app/api/views/events.py
+++ app/api/views/events.py
@@ -1,9 +1,13 @@
 """Views for events routers."""
 
-from fastapi import APIRouter, status
-from app.utils.constants import LINE_PROVIDER_URL
+from uuid import UUID
 
 import httpx
+from fastapi import APIRouter, status
+
+from app.api.services.bets import BetService
+from app.db.schemas import StatusUpdateSchema
+from app.utils.constants import LINE_PROVIDER_URL
 
 router = APIRouter()
 
@@ -13,3 +17,8 @@ async def list_event() -> list:
     async with httpx.AsyncClient() as client:
         response = await client.get(LINE_PROVIDER_URL + '/api/v1/events/')
     return response.json()
+
+
+@router.patch('/{uuid}/status', status_code=status.HTTP_204_NO_CONTENT)
+async def update_status(uuid: UUID, status_schema: StatusUpdateSchema) -> None:
+    return await BetService.update_status(event_uuid=uuid, event_status=status_schema.event_status)
