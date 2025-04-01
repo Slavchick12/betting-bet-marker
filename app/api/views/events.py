8070fd369d815595ebc7400ce10257c5b954f194
ocm <v.turukin@ocm-expert.com>
2025-04-01 13:05:52 +0300

    update_status docs cretated

--- app/api/views/events.py
+++ app/api/views/events.py
@@ -21,4 +21,5 @@ async def list_event() -> list:
 
 @router.patch('/{uuid}/status', status_code=status.HTTP_204_NO_CONTENT)
 async def update_status(uuid: UUID, status_schema: StatusUpdateSchema) -> None:
+    """Callback url for change event status."""
     return await BetService.update_status(event_uuid=uuid, event_status=status_schema.event_status)
